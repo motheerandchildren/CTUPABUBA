0ab8c7761ccffb12233dc20542f17c5d1d165d10
motheerandchildren <107709979+motheerandchildren@users.noreply.github.com>
2023-03-14 17:43:34 +0400

    AMOGUS

---
+++ POPKA_PATKA_GIROVAA_CKLADKA/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String  gopka = "амогус";
+        System.out.println(gopka+gopka);
+        System.out.println(gopka);
+    }
+}
